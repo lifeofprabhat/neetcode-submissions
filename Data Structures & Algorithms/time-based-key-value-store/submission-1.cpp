1cb93ab4e1214add722252c9f740394c1530e6e2
lifeofprabhat <prabhuon786@gmail.com>
2026-09-26 11:40:49 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,24 @@
+class TimeMap {
+public:
+    unordered_map<string, map<int,string>> mp;
+
+    TimeMap() {
+
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].insert({timestamp, value});
+        
+    }
+    
+    string get(string key, int timestamp) {
+        if(!mp.contains(key)) return "";
+        auto& mp2 = mp[key];
+        auto it = mp2.lower_bound(timestamp+1);
+        if(it == mp2.begin()) return "";
+        it--;
+        return it->second;
+        
+    }
+};
